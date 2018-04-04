bddc83e171f107150c8da7d6c56e3fe8b7fcbf5f
beulathamban <35990861+beulathamban@users.noreply.github.com>
2018-04-05 11:52:18 +1200

    student mark

---
+++ studmark.php
@@ -0,0 +1,16 @@
+<?php
+$stud=array(
+		array(
+				"name"=>"John",
+				"mod1"=>78,
+				"mod2"=>65
+			),
+		array(
+				"name"=>"Smith",
+				"mod1"=>58,
+				"mod2"=>85
+			),
+	);
+print_r($stud);
+
+?>
