95bc8cb6ee17af02235a28f446e40bf45506a44a
beulathamban <35990861+beulathamban@users.noreply.github.com>
2018-04-04 17:18:05 +1200

    receiving user input to run php

---
+++ diameter.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="common.css">
+</head>
+<body >
+<?php
+$r=$_POST["radius"];
+echo "radius=$r <br>";
+$d=2*$r;
+echo "diameter=$d<br>";
+?>
+</body>
+</html>
