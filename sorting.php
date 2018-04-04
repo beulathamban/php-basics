60c4fb581a88defdbbd7b61544ea5e940accafa7
beulathamban <35990861+beulathamban@users.noreply.github.com>
2018-04-05 11:49:19 +1200

    sorting

--- sorting.php
+++ sorting.php
@@ -1,19 +1,22 @@
 <?php
+
 $n=array(5,3,4,1,2);
-/*// echo $n[3];
+//sorting logic
 for($i=0; $i<=4; $i++) {
 	echo $n[i];
-	for($j=($i+1); $j<=3; $j++) {
+	for($j=($i+1); $j<=4; $j++) {
 		echo $n[j];
-		if ($n[i]>$n[j]) {
-			$temp=$n[i];
-			$n[i]=$n[j];
-			$n[j]=$temp;
-			echo $n[i], $n[j];
+		if ($n[$i]>$n[$j]) {
+			$temp=$n[$i];
+			$n[$i]=$n[$j];
+			$n[$j]=$temp;
+			
 		}
 	}
 }
-*/
+print_r($n);
+echo "<br>", "success <br>";
+//sort using pre-defined or built-in functions
 sort($n);
 print_r ($n);
 echo "<br>";
